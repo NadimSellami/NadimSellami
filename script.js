848196c3572f1691569e56fdc7c617e1c679ead6
NadimSellami <91118706+NadimSellami@users.noreply.github.com>
2021-10-19 10:44:13 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+fetch("header.html")
+     .then(contenu=>contenu.text())
+     .then(text=>{
+         document.getElementById("header").innerHTML = text;
+        })
+
+fetch("liens.html")
+    .then(contenu=>contenu.text())
+    .then(text=>{
+        document.getElementById("liens").innerHTML = text;
+    })
